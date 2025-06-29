9739a19c00f2a9631ac0bb7ed642c586980be5d3
makaron1225 <capricornusshota@gmail.com>
2025-06-29 20:42:48 +0900

    練習37まで

---
+++ practice32.py
@@ -0,0 +1,8 @@
+for i in range(1, 10):
+    print(i)
+
+for n in range(2):
+    print('kikagaku')
+
+for m in range(2):
+    print(m)
---
+++ practice36.py
@@ -0,0 +1,11 @@
+tpl = (1, 2, 3)
+print(tpl)
+print(tpl[0])
+
+tpl = ("P", "y", "t", "h", "o", "n")
+print("tpl[0]", tpl[0])  # 'P'
+print("tpl[1]", tpl[1])  # 'y'
+print("tpl[2]", tpl[2])  # 't'
+print("tpl[3]", tpl[3])  # 'h'
+print("tpl[4]", tpl[4])  # 'o'
+print("tpl[5]", tpl[5])  # 'n'
